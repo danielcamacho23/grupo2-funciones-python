93d5f98c915f22f2efd77a98045dbf54868a9646
danielcamacho23 <daniel_cmacho6@hotmail.com>
2025-10-30 15:13:15 -0300

    cambio en test

--- test/test_potenciacamacho.py
+++ test/test_potenciacamacho.py
@@ -1,5 +1,5 @@
 #tests/test_potencia.py
-from ..funciones.potenciacamacho import potencia
+from funciones.potenciacamacho import potencia
 def test_potencia():
     assert potencia(2, 3) == 8
     assert potencia(5, 0) == 1
